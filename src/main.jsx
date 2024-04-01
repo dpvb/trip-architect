de7fa0aa783ce3052411ecff352400b45bd188ed
Dylan van Bunnik <bungohdev@gmail.com>
2024-04-01 11:29:58 -0400

    remove old router.

--- src/main.jsx
+++ src/main.jsx
@@ -7,25 +7,6 @@ import PEPage from './pages/PEPage.jsx'
 import REPage from './pages/REPage.jsx'
 import IOPage from './pages/IOPage.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/trip-architect/',
-    element: <HomePage />,
-  },
-  {
-    path: '/trip-architect/itinerary-optimization',
-    element: <h1>Itinerary Optimization</h1>
-  },
-  {
-    path: '/trip-architect/prompt-engineering',
-    element: <PEPage />
-  },
-  {
-    path: '/trip-architect/response-evaluation',
-    element: <h1>Response Evaluation</h1>
-  },
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
